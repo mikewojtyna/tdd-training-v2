2fce943ed76bbe708aa0521bc968a82edf5a2d26
Mike Wojtyna <mike@buildmysoftware.pro>
2022-11-26 09:39:13 +0100

    Add katas and descriptions

---
+++ src/test/java/pl/wojtyna/trainings/tdd/TddTrainingV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojtyna.trainings.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTrainingV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
